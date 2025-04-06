3825e45917a92e66bbd02934b390fe59664f110a
Shehzad Shaikh <shaikhshehzad024@gmail.com>
2025-04-07 00:10:04 +0530

    Done

---
+++ src/drawgraphics.cpp
@@ -0,0 +1,8 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+
+int main(int argc, char *argv[]) {
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  return 0;
+}
--- src/getfont.cpp
+++ src/getfont.cpp
@@ -1,32 +1,36 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-std::vector<uint8_t> getfont() {
-  std::ifstream fontfile("fontset.txt");
-  if (!fontfile) {
-    std::cout << "Error opening file" << std::endl;
-    return {};
-  }
-  std::vector<uint8_t> fontset;
-  char bits[4];
-  char ch;
-  int count = 0;
-  while (fontfile.get(ch)) {
-    if (ch == '\n') {
-      count = 0;
-      continue;
-    }
-    if (ch == '0' || ch == '1') {
-      bits[count++] = ch;
-      if (count == 4) {
-        uint8_t number = ((bits[0] & 1) << 3) | ((bits[1] & 1) << 2) |
-                         ((bits[2] & 1) << 1) | ((bits[3] & 1) << 0);
-        number *= 10;
-        std::cout << static_cast<int>(number) << std::endl;
-        fontset.push_back(number);
-        count = 0;
-      }
-    }
-  }
-  return fontset;
+std::vector<uint8_t> getfont()
+{
+	std::ifstream fontfile("fontset.txt");
+	if (!fontfile)
+	{
+		std::cout << "Error opening file" << std::endl;
+		return {};
+	}
+	std::vector<uint8_t> fontset;
+	uint8_t chbuf[8];
+	uint8_t number;
+	char ch;
+	int count = 0;
+	while (fontfile.get(ch))
+	{
+		if (ch == '\n')
+		{
+			count = 0;
+			continue;
+		}
+		while (count < 8)
+		{
+			
+			if (ch == '0' || ch == '1')
+			{
+				chbuf[count] = (uint8_t) (ch-0);
+				count++;
+			}
+		}
+		
+	}
+return fontset;
 }
