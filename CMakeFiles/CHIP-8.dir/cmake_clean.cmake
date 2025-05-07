0a165e5d189cdad07158a7efe85610f2c11f6dd0
Shehzad Shaikh <shaikhshehzad024@gmail.com>
2025-05-07 18:37:54 +0530

    done

---
+++ CMakeFiles/CHIP-8.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CHIP-8"
+  "CHIP-8.pdb"
+  "CMakeFiles/CHIP-8.dir/src/drawgraphics.cpp.o"
+  "CMakeFiles/CHIP-8.dir/src/drawgraphics.cpp.o.d"
+  "CMakeFiles/CHIP-8.dir/src/fontset.cpp.o"
+  "CMakeFiles/CHIP-8.dir/src/fontset.cpp.o.d"
+  "CMakeFiles/CHIP-8.dir/src/getfont.cpp.o"
+  "CMakeFiles/CHIP-8.dir/src/getfont.cpp.o.d"
+  "CMakeFiles/CHIP-8.dir/src/loadrom.cpp.o"
+  "CMakeFiles/CHIP-8.dir/src/loadrom.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHIP-8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CHIP-8.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CHIP-8.
+# This may be replaced when dependencies are built.
